3c5abae688347b809207289c3dcc949f71a07ff4
Meher-Prabhu <meherprabhu18@gmail.com>
2015-03-15 02:20:14 +0530

    Third Commit
    Added location stuff in fareActivity need to check working

--- FareMeter/app/src/main/java/com/project/meher/faremeter/calculatorService.java
+++ FareMeter/app/src/main/java/com/project/meher/faremeter/calculatorService.java
@@ -1,26 +1,39 @@
 package com.project.meher.faremeter;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
 
 /**
  * Created by Meher on 14/03/2015.
  */
-public class calculatorService extends IntentService {
+public class calculatorService extends Service {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+    private final static String MESSAGE_KEY = "com.project.meher.calculator.SERVICE";
+    private final static int CALCULATE_START = 1;
+    private final static int CALCULATE_END = 2;
+    long waitTime = 0;
+    Location prevLocation;
+    Location currLocation;
+
+    final Messenger mMessenger = new Messenger(new IncomingMessagesHandler());
+
+    static class IncomingMessagesHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what) {
+
+            }
+            super.handleMessage(msg);
+        }
     }
 
-    public calculatorService(String name) {
-        super(name);
-    }
-
     @Override
-    protected void onHandleIntent(Intent intent) {
-
-
-
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
--- FareMeter/app/src/main/java/com/project/meher/faremeter/fareActivity.java
+++ FareMeter/app/src/main/java/com/project/meher/faremeter/fareActivity.java
@@ -1,21 +1,67 @@
 package com.project.meher.faremeter;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class fareActivity extends ActionBarActivity {
+
+    TextView locView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fare);
 
+        locView = (TextView) findViewById(R.id.locationView);
+
         Intent call = getIntent();
         String vehicle = call.getStringExtra("Vehicle");
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+// Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener(){
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                makeUseOfNewLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+// Register the listener with the Location Manager to receive location updates
+        Location lastLocation;
+        if((lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)) == null)
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        else {
+            Toast.makeText(getApplicationContext(), "YO MAX", Toast.LENGTH_SHORT).show();
+            locView.setText("YO MAX");
+            //locView.setText(lastLocation.toString());
+        }
+
+
+        //locView.setText(loc.toString());
+
+    }
+
+    void makeUseOfNewLocation(Location location) {
+        //locView.setText(location.toString());
+        Toast.makeText(getApplicationContext(),"YO",Toast.LENGTH_SHORT).show();
+        locView.setText("Yo");
     }
 
 
